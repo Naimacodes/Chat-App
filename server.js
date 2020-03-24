04058f81cebb93452743858952644c5042122d6c
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-24 14:47:31 +0100

    added project's structure, and set static folder on server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+const path = require('path')
 const express = require('express')
 
 const app = express();
+
+//set static folder for our front end
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 
 const PORT = 3000 || process.env.PORT;
 
