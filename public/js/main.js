958e302ee0a04c04b8b01921c17a916d3e74541b
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-24 21:19:16 +0100

    added qs libray to join specific chat room

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,16 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 
+//get username and room from URL
+const {username, room} = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+})
+
 const socket = io();
+
+//Join chatroom
+
+socket.emit('joinRoom', {username, room)
 
 socket.on('message', message => {
   console.log(message);
@@ -35,7 +44,7 @@ function outputMessage(message) {
   const div = document.createElement('div');
   div.classList.add('message');
   div.innerHTML = `
-  <p class="meta">Naima<span>9:00 am</span></p>
-  <p class="text">${message}</p>`;
+  <p class="meta">${message.username}<span>${message.time}</span></p>
+  <p class="text">${message.text}</p>`;
   document.querySelector('.chat-messages').appendChild(div);
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
@@ -10,26 +11,31 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'Admin';
+
 // runs when client connects, listens to an event
 
 io.on('connection', socket => {
   //welcomes current user
-  socket.emit('message', 'Welcome to the chat');
+  socket.emit('message', formatMessage(botName, 'Welcome to the chat'));
 
   //broadcasts to everybody when a certain user connects except the user in question
-  socket.broadcast.emit('message', 'a user has joined the chat');
+  socket.broadcast.emit(
+    'message',
+    formatMessage(botName, 'a user has joined the chat')
+  );
 
   // runs when client disconnects
   socket.on('disconnect', () => {
     //emits to everyone
-    io.emit('message', 'a user has left the chat');
+    io.emit('message', formatMessage(botName, 'a user has left the chat'));
   });
 
   //listens for chatMessage
   socket.on('chatMessage', msg => {
     //emits the message back to the client for everyone to see
 
-    io.emit('message', msg)
+    io.emit('message', formatMessage('user', msg));
   });
 });
 
