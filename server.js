00efc64449cce4e1cdd798b0a05f58993f045ee1
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-24 16:55:42 +0100

    initialized socket.io

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const path = require('path')
 const express = require('express')
-
+const socketio = require('socket.io')
+const http = require('http')
 const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
 
 //set static folder for our front end
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+// run when a client connects, listens to an event
+
+io.on('connection', socket=> {
+  console.log('new web socket ');
+})
+
 
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT} `))
+server.listen(PORT, () => console.log(`server running on port ${PORT} `))
